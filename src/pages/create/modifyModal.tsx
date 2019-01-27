fa695d584ca10ac3f8d9d2224a8d201518ee54fc
Isaiah Kahler <isaiahwkahler@gmail.com>
2019-01-27 13:55:53 -0500

    add event button

--- src/pages/create/modifyModal.tsx
+++ src/pages/create/modifyModal.tsx
@@ -1,56 +1,76 @@
-import * as React from 'react';
-import { Modal } from '../../components/modal';
-import { Dropdown } from '../../components/dropdown';
-import styled from 'styled-components';
-import { Input } from '../../components/input';
-import { Song, Reading, Plain, Cover, Option } from '../../components/models';
-
+import * as React from "react";
+import { Modal } from "../../components/modal";
+import { Dropdown } from "../../components/dropdown";
+import styled from "styled-components";
+import { Input } from "../../components/input";
+import { Song, Reading, Plain, Cover, Option, SongTemplate, ReadingTemplate, CoverTemplate, PlainTemplate } from "../../components/models";
+import { Form } from "../../components/form";
 
 const Label = styled.h2`
   margin: 10px 0 0 0;
 `;
 
 interface ModifyModalProps {
-    type: "add event" | "order events",
-    onClose: Function,
-    onSubmit: (program) => void,
-    program: Array<Reading | Song | Plain | Cover>
+  type: "add event";
+  onClose: Function;
+  onSubmit: (program) => void;
+  program: Array<Reading | Song | Plain | Cover>;
 }
 
 interface ModifyModalState {
-    type: 'reading' | 'song' | 'plain' | 'cover',
-
+  type: "reading" | "song" | "plain" | "cover";
 }
 
-export class ModifyModal extends React.Component<ModifyModalProps, ModifyModalState> {
-    constructor(props: ModifyModalProps) {
-        super(props);
-        this.state = { type: "plain" }
-    }
+export class ModifyModal extends React.Component<
+  ModifyModalProps,
+  ModifyModalState
+> {
+  constructor(props: ModifyModalProps) {
+    super(props);
+    this.state = {
+      type: "plain"
+    };
+  }
 
-    componentDidUpdate() {
-        console.log(this.state.type)
-    }
+  componentDidUpdate() {
+    console.log(this.state.type);
+  }
 
-    render() {
-        return (
-            <Modal title={this.props.type} onClose={this.props.onClose}>
-                {this.props.type === "add event" ? (
-                    <React.Fragment>
-                        <Label>Choose Event Type</Label>
-                        <Dropdown value={this.state.type} options={['plain', 'song', 'reading', 'cover']} onSubmit={(value) => this.setState({ type: value })} submitOnChange />
-                        {/* <Input
-                            defaultValue={'add'}
-                            placeholder={item.display}
-                            onSubmit={value => this.props.submitHandler(item.dataname, value)}
-                            update={this.state.update}
-                        /> */}
-                    </React.Fragment>
-                ) : (
-                        <div></div>
-                    )}
-            </Modal>
-
-        );
+  submit = () => {
+    let newEvent:Reading | Song | Plain | Cover = null;
+    if(this.state.type === "song"){
+        newEvent = {...SongTemplate};
+    } else if(this.state.type === "reading"){
+        newEvent = {...ReadingTemplate};
+    } else if(this.state.type === "cover") {
+        newEvent = {...CoverTemplate};
+    } else {
+        newEvent = {...PlainTemplate};
     }
+    let newProgram = this.props.program;
+    newProgram.push(newEvent);
+    this.props.onSubmit(newProgram)
+    this.props.onClose()
+  };
+
+  render() {
+    return (
+      <Modal title={this.props.type} onClose={this.props.onClose}>
+          <React.Fragment>
+            <Label>Choose Event Type</Label>
+            <Dropdown
+              value={this.state.type}
+              options={["plain", "song", "reading", "cover"]}
+              onSubmit={value => this.setState({ type: value })}
+              submitOnChange
+            />
+            <Form
+              onSubmit={this.submit}
+              submitButton={true}
+              submitButtonValue="Add Event"
+            />
+          </React.Fragment>
+      </Modal>
+    );
+  }
 }
